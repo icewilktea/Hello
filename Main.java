ac36e418de891d27cdbf539a36fb6cfdbf65e4e7
icewilktea <59664590+icewilktea@users.noreply.github.com>
2020-10-05 13:10:34 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+    public static void main(String[] args) {
+        Scanner inpt = new Scanner(System.in);
+        System.out.println("something");
+        String thing = inpt.nextLine();
+        System.out.println("Hello world!"+thing);
   }
 }
